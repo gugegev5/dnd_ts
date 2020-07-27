e3f936e92e3415021f53aeb64939dc3bca7dd6b3
gup <gupeng@qie.tv>
2020-07-27 18:44:04 +0800

    feat: build

---
+++ build/precache-manifest.844170e8321a3e0652b0b316384fd4b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db59564e32227e02d014423315c2d289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73855f22f01ce0f6ebe",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "3da85e575ccd7956cbed",
+    "url": "/static/css/main.5db5b2d9.chunk.css"
+  },
+  {
+    "revision": "e73855f22f01ce0f6ebe",
+    "url": "/static/js/2.f51bce9b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f51bce9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da85e575ccd7956cbed",
+    "url": "/static/js/main.ccc2706a.chunk.js"
+  },
+  {
+    "revision": "4a9410aa3d6e14f18b64",
+    "url": "/static/js/runtime-main.9ef963f0.js"
+  }
+]);
